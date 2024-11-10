2965d2924f1748d293f46d3eb72cbc4d0d034d6f
den4k <den4k@DESKTOP-UFR7S8E>
2024-11-10 15:20:43 +0300

    Add project files.

---
+++ console_practic_5/console_practic_5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>;
+#include <windows.h>
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	int x, repeat;
+	do
+	{
+		cout << "Введіть x:";
+		cin >> x;
+		switch (x) {
+		case 1: cout << "Шуруп.\n"; break;
+		case 2: cout << "Гайка.\n"; break;
+		case 3: cout << "Гвинт.\n"; break;
+		case 4: cout << "Цвях.\n"; break;
+		case 5: cout << "Болт.\n"; break;
+		default: cout << "Деталь не знайдено.\n";
+		}
+		cout << "Повторити пошук? (1 - так)" << endl;
+		cin >> repeat;
+	} while (repeat == 1);
+	return 0;
+}
+
